547e2c8a81d8b925f85ab73d299ecc041e2d9d5a
zaki azizi <zakiazizi1841992@gmail.com>
2021-08-07 10:05:45 +0200

    first commit

---
+++ firebase.js
@@ -0,0 +1,14 @@
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCttVpbUE-2t062FQTFr5WNMXKciTorIbg",
+    authDomain: "clone-2-dc0c7.firebaseapp.com",
+    projectId: "clone-2-dc0c7",
+    storageBucket: "clone-2-dc0c7.appspot.com",
+    messagingSenderId: "774781737060",
+    appId: "1:774781737060:web:5efc2038c421d448ec2771"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
